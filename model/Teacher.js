fa9c8bd5a94360ba7673f5cc269ac6b10c3c0804
wfrotj <trebrodrigo@gmail.com>
2023-08-07 23:48:48 +0800

    fist commit

---
+++ model/Teacher.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose, { plugin } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+const teacherSchema = new mongoose.Schema({
+  username: "String",
+  passwordHash: String,
+  firstName: String,
+  lastName: String,
+  middleInitial: String,
+  age: Number,
+  employeeNumber: String,
+  depEdEmail: String,
+});
+teacherSchema.plugin(uniqueValidator);
+const Teacher = mongoose.model("Teacher", teacherSchema);
+
+export default Teacher;
